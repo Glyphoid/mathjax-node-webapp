9bd97052d15793ed5bf0b5393b413e9ab16a2d37
Shanqing Cai <shanqing.cai@gmail.com>
2016-07-11 14:08:55 +0000

    Chaser

--- mathjax-server.js
+++ mathjax-server.js
@@ -4,6 +4,11 @@ var MathJaxDelegate = require("./mathjax-delegate.js")
 var servingPath = "/math-jax/math-jax";
 var servingContentType = "application/json";
 
+var responseHeader = {
+    "Content-Type": servingContentType,
+    "Access-Control-Allow-Origin": "*"
+};
+
 var respErrorsKey = "errors";
 
 mjDelegate = new MathJaxDelegate()
@@ -22,20 +27,28 @@ server = http.createServer(function(req, resp) {
 
 		    mjDelegate.convert(mjInputObj, function(data) {
 			if (data.errors) {
-			    resp.writeHead(500,
-					   {"Content-Type":
-					    servingContentType});
-			    resp.end({
+			    resp.writeHead(500, responseHeader);
+			    resp.end(JSON.stringify({
 				respErrorsKey: ["Error occurred during " +
 					        "conversion"]
-			    });
+			    }));
 			} else {
 			    outObj = {
 				"mathTex": mjInputObj.mathTex,
 			    };
 
 			    if (mjInputObj.imageFormat == "png") {
+				// TODO: Remove ulgy logic
+				var png_data_prefix = "data:image/png;base64,";
 				outObj.conversionResult = data.png;
+
+				if (outObj.conversionResult.substring(0, png_data_prefix.length) ===
+				    png_data_prefix) {
+				    outObj.conversionResult = 
+					outObj.conversionResult.substring(png_data_prefix.length,
+									  outObj.conversionResult.length);
+				}
+
 			    } else if (mjInputObj.imageFormat == "MathML") {
 				outObj.conversionResult = data.mml;
 			    } else {
@@ -43,25 +56,22 @@ server = http.createServer(function(req, resp) {
 			    }
 
 			    if (typeof outObj.conversionResult == "string") {
-				resp.writeHead(200,
-					       {"Content-Type":
-						servingContentType});
+				resp.writeHead(200, responseHeader);
 				resp.end(JSON.stringify(outObj));
 			    } else {
-				resp.writeHead(400,
-					       {"Content-Type":
-						servingContentType});
-				resp.end({
+				resp.writeHead(400, responseHeader);
+				console.error("typeof outObj.conversionResult =",
+					      (typeof outObj.conversionResult));
+				resp.end(JSON.stringify({
 				    respErrorsKey:
 				      ["Unsupported conversion type"]
-				});
+				}));
 			    }
 			}
 		    });
 		} catch (e) {
 		    console.log("Parsing failed");			
-		    resp.writeHead(400,
-				   {"Content-Type": servingContentType});
+		    resp.writeHead(400, responseHeader);
 		    resp.end(JSON.stringify({
 			respErrorsKey: ["Invalid JSON content in request"]
 		    }));
@@ -70,13 +80,13 @@ server = http.createServer(function(req, resp) {
 		
 	    });
 	} else {
-	    resp.writeHead(404, {"Content-Type": servingContentType});
+	    resp.writeHead(404, responseHeader);
 	    resp.end(JSON.stringify({
 		respErrorsKey: ["Invalid request path"]
 	    }));
 	}
     } else {
-	resp.writeHead(404, {"Content-Type": servingContentType});
+	resp.writeHead(404, responseHeader);
 	resp.end(JSON.stringify({
 	    respErrorsKey: ["Invalid request method"]
 	}));
