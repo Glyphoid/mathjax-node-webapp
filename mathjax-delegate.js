220c0ed32f9aa45c1e2325f04fd73446ae872e89
shanqing.cai <shanqing.cai@gmail.com>
2016-07-10 23:08:18 -0400

    Initial commit

---
+++ mathjax-delegate.js
@@ -0,0 +1,47 @@
+var mjAPI = require("mathjax-node/lib/mj-single.js");
+
+var MathJaxDelegate = function() {
+    var self = this;
+
+    // Start mathjax API
+    mjAPI.start();
+
+    // Input json follows the format, e.g.,
+    // {
+    //     "mathTex": "E = mc^2",
+    //     "imageFormat": "png",  // Or "MathML"
+    //     "imageWidth": 300,
+    //     "imageDpi": 200,
+    // }
+    // Args:
+    //   input: Input JSON object
+    //   callback: Callback for MathJax typeset completion
+    this.convert = function(input, callback) {
+	// TODO: Sanity-check that callback is a function
+	
+	mjInput = {
+	    math: input.mathTex,
+	    format: "TeX"
+	};
+
+	if (input.imageFormat == "png") {
+	    mjInput.png = true;
+	    mjInput.width = input.imageWidth;
+	    mjInput.dpi = input.imageDpi;
+	} else if (input.imageFormat ="MathML") {
+	    mjInput.mml = true;
+	} else {
+	    // TODO: Throw error?
+	}
+
+	mjAPI.typeset(mjInput, function(data) {
+	    callback(data);
+	});
+    };
+
+    this.sayHello = function() {
+	console.log("Hello");
+    };
+};
+
+module.exports = MathJaxDelegate;
---
+++ mathjax-server.js
@@ -0,0 +1,90 @@
+var http = require("http")
+var MathJaxDelegate = require("./mathjax-delegate.js")
+
+var servingPath = "/math-jax/math-jax";
+var servingContentType = "application/json";
+
+var respErrorsKey = "errors";
+
+mjDelegate = new MathJaxDelegate()
+
+server = http.createServer(function(req, resp) {
+    if (req.method == "POST") {
+	if (req.url == servingPath) {
+	    var reqBody = "";
+	    req.on("data", function(data) {
+		reqBody += data;
+	    });
+	    
+	    req.on("end", function() {
+		console.log("Parsing reqBody");  // DEBUG
+		try {
+		    mjInputObj = JSON.parse(reqBody);
+
+		    mjDelegate.convert(mjInputObj, function(data) {
+			if (data.errors) {
+			    resp.writeHead(500,
+					   {"Content-Type":
+					    servingContentType});
+			    resp.end({
+				respErrorsKey: ["Error occurred during " +
+					        "conversion"]
+			    });
+			} else {
+			    outObj = {
+				"mathTex": mjInputObj.mathTex,
+			    };
+
+			    if (mjInputObj.imageFormat == "png") {
+				outObj.conversionResult = data.png;
+			    } else if (mjInputObj.imageFormat == "MathML") {
+				outObj.conversionResult = data.mml;
+			    } else {
+				outObj.conversoinResult = undefined;
+			    }
+
+			    if (typeof outObj.conversionResult == "string") {
+				resp.writeHead(200,
+					       {"Content-Type":
+						servingContentType});
+				resp.end(JSON.stringify(outObj));
+			    } else {
+				resp.writeHead(400,
+					       {"Content-Type":
+						servingContentType});
+				resp.end({
+				    respErrorsKey:
+				      ["Unsupported conversion type"]
+				});
+			    }
+			}
+		    });
+		} catch (e) {
+		    console.log("Parsing failed");			
+		    resp.writeHead(400,
+				   {"Content-Type": servingContentType});
+		    resp.end(JSON.stringify({
+			respErrorsKey: ["Invalid JSON content in request"]
+		    }));
+		}
+		
+		
+	    });
+	} else {
+	    resp.writeHead(404, {"Content-Type": servingContentType});
+	    resp.end(JSON.stringify({
+		respErrorsKey: ["Invalid request path"]
+	    }));
+	}
+    } else {
+	resp.writeHead(404, {"Content-Type": servingContentType});
+	resp.end(JSON.stringify({
+	    respErrorsKey: ["Invalid request method"]
+	}));
+    }
+});
+
+// TODO: Turn into command-line arguments
+port = 80;
+host = "localhost";
+server.listen(port, host);
